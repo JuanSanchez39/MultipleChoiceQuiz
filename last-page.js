edbd7286ac2828aa98f94691967fc3b3b346e0b7
JuanSanchez39 <juan_sanchez39@hotmail.com>
2023-04-27 20:23:13 -0400

    created Highscores html and starting on the JS and CSS , Finished a few things in last-page js and html

---
+++ last-page.js
@@ -0,0 +1,35 @@
+const username = document.querySelector('#username');
+const saveScoreBtn = document.querySelector('#saveScoreBtn');
+const finalScore = document.querySelector('#finealScore');
+const mostRecentScore = document.querySelector('#mostRecentScore');
+
+const highScores =JSON.parse(localStorage.getItem("highScores")) || [];
+
+const MAX_HIGH_SCORES =5;
+
+finalScore.innerText = mostRecentScore;
+
+username.addEventListener("keyup", () => {
+    saveScoreBtn.disabled = !username.ariaValueMax;     //allows to save once input was set in.
+})
+
+ saveHighScore = e => {
+    e.preventDefault ()
+
+    const score = {
+        score : mostRecentScore,
+        name: username.value
+    }
+
+
+    highScores.push(score);
+
+    highScores.sorts((a,b) => { 
+        return b.score - a.score;
+    })
+
+    highScores.splice(5);
+
+    localStorage.setItem('highScores', JSON.stringify(highScores));
+    window.location.assign("");
+}
